800ab62f46da61100cce8b3f43b8c2b29d5cd976
DrAnaconda <anonymouls777@gmail.com>
2024-01-08 02:40:03 +0200

    Initial commit

---
+++ LootScannerKeygen/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+
+Console.Write("Copy public key: ");
+var machineRegexGuid = Console.ReadLine();
+
+
+var daysToExpire = 0;
+Console.Write($"Days to expire [Default (enter/empty) is 30 days]: ");
+while (true)
+{
+    try
+    {
+        var userInput = Console.ReadLine();
+        daysToExpire = string.IsNullOrWhiteSpace(userInput) ? 30 : int.Parse(userInput);
+        break;
+    }
+    catch (Exception e)
+    {
+        Console.Write($"Suka dolbaeb print like a human not like dalbaeb: {e.Message}");
+    }
+}
+
+var aesAlg = Aes.Create();
+aesAlg.IV = "abcdef9876543210"u8.ToArray();
+aesAlg.Key = "0123456789abcdef"u8.ToArray();
+
+// Regex is: ---Days:(\d+) --- Y:(\d+) --- M:(\d+) --- D:(\d+) --- Machine:(.+)
+var startActivationDate = DateTime.UtcNow;
+var originalText = $"---Days:{daysToExpire} --- Y:{startActivationDate.Year} --- M:{startActivationDate.Month} --- D:{startActivationDate.Day} --- Machine:{machineRegexGuid}";
+
+using var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+using var msEncrypt = new MemoryStream();
+using var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+using (var swEncrypt = new StreamWriter(csEncrypt))
+{
+    swEncrypt.Write(originalText);
+}
+
+var encrypted = msEncrypt.ToArray();
+
+var encryptedText = Convert.ToBase64String(encrypted);
+Console.WriteLine("Activation key is below. Copy, send and close this window. User should put this line in the \"Activation key\" field");
+Console.WriteLine(encryptedText);
+Console.ReadLine();
---
+++ Readme.md
@@ -0,0 +1,26 @@
+﻿# Keygen for LootScanner plugin based on the ExileAPI
+
+## Analysis history
+
+1. Original is `LootScanner.dll`
+2. Two different obfuscators was applied (already was hacked)?
+3. Was found certificate pinning method which always returns true and doesn't called to somewhere (hacked before?)
+4. Current license handling is: Duration of key + machine id + AES
+5. Applied anti-tamper. In case change of code it will crash silently
+6. Good obfuscator was applied. Automated tools failed to deobfuscate.
+
+## Reverse history
+
+### Manual analysis with debugging ✅
+
+1. Retrieved encryption key for activation key decryption 
+2. Retrieved pattern of content of activation key: `---Days:(\d+) --- Y:(\d+) --- M:(\d+) --- D:(\d+) --- Machine:(.+)`
+
+Result: success. Based on analysis keygen was written.
+
+### Aggressive codebase change ❌
+
+1. Change method `GetCryptoToken` to always return same string
+2. Change field `CryptoTokenValid` to always return same string
+
+Result: failed. Unable to found anti tamper. Application crashes silently. I think stack overflow exception being raised.
